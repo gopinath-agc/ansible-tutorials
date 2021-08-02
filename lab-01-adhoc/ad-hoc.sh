5da6d7fb4785d9a762286e3b3b89320b7ad60d1e
gopinath-agc <gopinath.agc@gmail.com>
2021-08-02 16:38:41 -0500

    updated the node name in playbook

--- lab-01-adhoc/ad-hoc.sh
+++ lab-01-adhoc/ad-hoc.sh
@@ -6,12 +6,12 @@
 # ansible node01   -a "hostname -f"
 
 # ssh node01 "mkdir /home/ec2-user/mytestdir"
-ansible node01  -a "mkdir /home/ec2-user/testdirAnsible"
+ansible node01  -a "mkdir -p /home/ec2-user/testdirAnsible"
 ansible node01 -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
 ansible node01 -m shell -a "cat /tmp/ansible-nodes" 
 ansible node01 -b -m yum -a "name=httpd state=present"
 ansible node01 -b -m yum -a "name=git state=present"
-ansible node01 -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp version=HEAD"
+ansible node01 -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp1 version=HEAD"
 
 # Using Ansible modules in ad-hoc commands
 ansible -m -m yum -a "name=nginx state=present" node01 --become
